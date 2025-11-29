bfaf876d030e264c4e5af72284b135c4446bed6d
Hibara-cloud <bayan731e@gmail.com>
2025-11-29 18:24:21 +0300

    Update live_bot.py

--- live_bot.py
+++ live_bot.py
@@ -10,7 +10,7 @@ import time
 import logging
 import math
 import getpass # Used for secure password input
-from ExpertOptionAPI import ExpertOption
+from ExpertOptionAPI.client import ExpertOption
 
 # ---------- 🚨 Trading Settings (No Credentials Here) 🚨 ----------
 ASSET_ID = "EURUSD"                     # Trading Asset
