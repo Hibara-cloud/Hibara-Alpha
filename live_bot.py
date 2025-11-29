bebdf7aa858a22b51e8b44dce5f999e963ca2459
Hibara-cloud <bayan731e@gmail.com>
2025-11-29 18:08:37 +0300

    Update live_bot.py

--- live_bot.py
+++ live_bot.py
@@ -10,7 +10,8 @@ import time
 import logging
 import math
 import getpass # Used for secure password input
-from ExpertOptionAPI.api import ExpertOption
+from ExpertOptionAPI.client import ExpertOption
+
 
 
 
